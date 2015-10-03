66b9d448c8a62ff05f2e0f1c0a80d4fca99f198c
Torsten Burschka <torsten.burschka@gmail.com>
2015-10-03 23:49:07 +0200

    Initial commit Added user entity implementation

---
+++ Entity/User.php
@@ -0,0 +1,407 @@
+<?php
+
+namespace IPC\SecurityBundle\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Serializable;
+use Symfony\Component\Security\Core\Role\Role;
+use Symfony\Component\Security\Core\Role\RoleInterface;
+use Symfony\Component\Security\Core\User\AdvancedUserInterface;
+use Symfony\Component\Security\Core\User\EquatableInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class User implements AdvancedUserInterface, EquatableInterface, Serializable
+{
+
+    /**
+     * Username
+     *
+     * @var string
+     */
+    protected $username;
+
+    /**
+     * Plain password
+     *
+     * @var string
+     */
+    protected $plainPassword;
+
+    /**
+     * Password
+     *
+     * @var string
+     */
+    protected $password;
+
+    /**
+     * Salt
+     *
+     * @var string
+     */
+    protected $salt;
+
+    /**
+     * Roles
+     *
+     * @var Collection|Role[]
+     */
+    protected $roles;
+
+    /**
+     * User expired
+     *
+     * @var bool
+     */
+    protected $expired;
+
+    /**
+     * User locked
+     *
+     * @var bool
+     */
+    protected $locked;
+
+    /**
+     * User enabled
+     *
+     * @var bool
+     */
+    protected $enabled;
+
+    /**
+     * Credentials expired
+     *
+     * @var bool
+     */
+    protected $credentialsExpired;
+
+    public function __construct()
+    {
+        $this->roles = new ArrayCollection();
+        $this->expired = false;
+        $this->locked = false;
+        $this->enabled = false;
+        $this->credentialsExpired = false;
+        $this->plainPassword = null;
+    }
+
+    /**
+     * Returns the roles granted to the user.
+     *
+     * <code>
+     * public function getRoles()
+     * {
+     *     return array('ROLE_USER');
+     * }
+     * </code>
+     *
+     * Alternatively, the roles might be stored on a ``roles`` property,
+     * and populated in any number of different ways when the user object
+     * is created.
+     *
+     * @return Role[] The user roles
+     */
+    public function getRoles()
+    {
+        return $this->roles;
+    }
+
+    /**
+     * @param Role|string $role
+     * @return $this
+     */
+    public function addRole($role)
+    {
+        if (is_string($role)) {
+            $role = new Role($role);
+        } elseif (!$role instanceof RoleInterface) {
+            throw new \InvalidArgumentException(
+                sprintf('$role must be a string, or RoleInterface instance, but got %s.', gettype($role))
+            );
+        } // no else
+        if (!$this->roles->containsKey($role->getRole())) {
+            $this->roles->set($role->getRole(), $role);
+        } // no else
+        return $this;
+    }
+
+    /**
+     * @param Role|string $role
+     * @return $this
+     */
+    public function removeRole($role)
+    {
+        if (is_string($role)) {
+            $role = new Role($role);
+        } elseif (!$role instanceof RoleInterface) {
+            throw new \InvalidArgumentException(
+                sprintf('$role must be a string, or RoleInterface instance, but got %s.', gettype($role))
+            );
+        }
+        if ($this->roles->containsKey($role->getRole())) {
+            $this->roles->remove($role->getRole());
+        } // no else
+        return $this;
+    }
+
+    /**
+     * @param string $plainPassword
+     * @return $this
+     */
+    public function setPlainPassword($plainPassword)
+    {
+        $this->plainPassword = $plainPassword;
+        return $this;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getPlainPassword()
+    {
+        return $this->plainPassword;
+    }
+
+    /**
+     * @param string $password
+     * @return $this
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+        return $this;
+    }
+
+    /**
+     *
+     * @return string The password
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $salt
+     * @return $this
+     */
+    public function setSalt($salt)
+    {
+        $this->salt = $salt;
+        return $this;
+    }
+
+    /**
+     * Returns the salt that was originally used to encode the password.
+     *
+     * This can return null if the password was not encoded using a salt.
+     *
+     * @return string|null The salt
+     */
+    public function getSalt()
+    {
+        return $this->salt;
+    }
+
+    /**
+     * @param string $username
+     * @return $this
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+        return $this;
+    }
+
+    /**
+     * Returns the username used to authenticate the user.
+     *
+     * @return string The username
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Removes sensitive data from the user.
+     *
+     * This is important if, at any given point, sensitive information like
+     * the plain-text password is stored on this object.
+     */
+    public function eraseCredentials()
+    {
+        $this->setPlainPassword(null);
+    }
+
+    /**
+     * Checks whether the user's account has expired.
+     *
+     * Internally, if this method returns false, the authentication system
+     * will throw an AccountExpiredException and prevent login.
+     *
+     * @return bool true if the user's account is non expired, false otherwise
+     *
+     * @see AccountExpiredException
+     */
+    public function isAccountNonExpired()
+    {
+        return !$this->isExpired();
+    }
+
+    /**
+     * @return bool
+     */
+    public function isExpired()
+    {
+        return $this->expired;
+    }
+
+    /**
+     * @param bool $expired
+     * @return $this
+     */
+    public function setExpired($expired)
+    {
+        $this->expired = $expired;
+        return $this;
+    }
+
+    /**
+     * Checks whether the user is locked.
+     *
+     * Internally, if this method returns false, the authentication system
+     * will throw a LockedException and prevent login.
+     *
+     * @return bool true if the user is not locked, false otherwise
+     *
+     * @see LockedException
+     */
+    public function isAccountNonLocked()
+    {
+        return !$this->isLocked();
+    }
+
+    /**
+     * @return bool
+     */
+    public function isLocked()
+    {
+        return $this->locked;
+    }
+
+    /**
+     * @param bool $locked
+     * @return $this
+     */
+    public function setLocked($locked)
+    {
+        $this->locked = $locked;
+        return $this;
+    }
+
+    /**
+     * Checks whether the user's credentials (password) has expired.
+     *
+     * Internally, if this method returns false, the authentication system
+     * will throw a CredentialsExpiredException and prevent login.
+     *
+     * @return bool true if the user's credentials are non expired, false otherwise
+     *
+     * @see CredentialsExpiredException
+     */
+    public function isCredentialsNonExpired()
+    {
+        return !$this->isCredentialsExpired();
+    }
+
+    /**
+     * @return bool
+     */
+    public function isCredentialsExpired()
+    {
+        return $this->credentialsExpired;
+    }
+
+    /**
+     * @param bool $credentialsExpired
+     * @return $this
+     */
+    public function setCredentialsExpired($credentialsExpired)
+    {
+        $this->credentialsExpired = $credentialsExpired;
+        return $this;
+    }
+
+    /**
+     * Checks whether the user is enabled.
+     *
+     * Internally, if this method returns false, the authentication system
+     * will throw a DisabledException and prevent login.
+     *
+     * @return bool true if the user is enabled, false otherwise
+     *
+     * @see DisabledException
+     */
+    public function isEnabled()
+    {
+        return $this->enabled;
+    }
+
+    /**
+     * @param bool $enabled
+     * @return $this
+     */
+    public function setEnabled($enabled)
+    {
+        $this->enabled = $enabled;
+        return $this;
+    }
+
+    public function serialize()
+    {
+        return serialize([
+            $this->username,
+            $this->password,
+            $this->salt,
+            $this->locked,
+            $this->expired,
+            $this->credentialsExpired,
+            $this->enabled,
+            $this->roles->toArray()
+        ]);
+    }
+
+    public function unserialize($serialized)
+    {
+        list(
+            $this->username,
+            $this->password,
+            $this->salt,
+            $this->locked,
+            $this->expired,
+            $this->credentialsExpired,
+            $this->enabled,
+            $roles
+        ) = unserialize($serialized);
+        $this->roles = new ArrayCollection($roles);
+    }
+
+    public function isEqualTo(UserInterface $user)
+    {
+        $userKey = implode('', [
+            $user->getUsername(),
+            $user->getPassword(),
+            $user->getSalt(),
+        ]);
+        $selfKey = implode('', [
+            $this->getUsername(),
+            $this->getPassword(),
+            $this->getSalt(),
+        ]);
+        return $userKey === $selfKey;
+    }
+
+}
---
+++ Tests/Entity/UserTest.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace IPC\SecurityBundle\Tests\Entity;
+
+use IPC\SecurityBundle\Entity\User;
+use PHPUnit_Framework_TestCase;
+use Symfony\Component\Security\Core\Role\Role;
+
+class UserTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var User
+     */
+    private $user;
+
+    public function setUp()
+    {
+        $this->user = new User();
+    }
+
+    public function testUsernameMethods()
+    {
+        $this->assertNull($this->user->getUsername());
+        $this->user->setUsername('username');
+        $this->assertEquals('username', $this->user->getUsername());
+    }
+
+    public function testPlainPasswordMethods()
+    {
+        $this->assertNull($this->user->getPlainPassword());
+        $this->user->setPlainPassword('plain_password');
+        $this->assertEquals('plain_password', $this->user->getPlainPassword());
+    }
+
+    public function testPasswordMethods()
+    {
+        $this->assertNull($this->user->getPassword());
+        $this->user->setPassword('password');
+        $this->assertEquals('password', $this->user->getPassword());
+    }
+
+    public function testSaltMethods()
+    {
+        $this->assertNull($this->user->getSalt());
+        $this->user->setSalt('salt');
+        $this->assertEquals('salt', $this->user->getSalt());
+    }
+
+    public function testEraseCredentialsMethods()
+    {
+        $this->user->setPlainPassword('plain_password');
+        $this->user->setPassword('password');
+        $this->user->eraseCredentials();
+        $this->assertNull($this->user->getPlainPassword());
+        $this->assertEquals('password', $this->user->getPassword());
+    }
+
+    public function testExpiredMethods()
+    {
+        $this->assertFalse($this->user->isExpired());
+        $this->assertTrue($this->user->isAccountNonExpired());
+        $this->user->setExpired(true);
+        $this->assertTrue($this->user->isExpired());
+        $this->assertFalse($this->user->isAccountNonExpired());
+    }
+
+    public function testLockedMethods()
+    {
+        $this->assertFalse($this->user->isLocked());
+        $this->assertTrue($this->user->isAccountNonLocked());
+        $this->user->setLocked(true);
+        $this->assertTrue($this->user->isLocked());
+        $this->assertFalse($this->user->isAccountNonLocked());
+    }
+
+    public function testEnabledMethods()
+    {
+        $this->assertFalse($this->user->isEnabled());
+        $this->user->setEnabled(true);
+        $this->assertTrue($this->user->isEnabled());
+    }
+
+    public function testCredentialsExpiredMethods()
+    {
+        $this->assertFalse($this->user->isCredentialsExpired());
+        $this->assertTrue($this->user->isCredentialsNonExpired());
+        $this->user->setCredentialsExpired(true);
+        $this->assertTrue($this->user->isCredentialsExpired());
+        $this->assertFalse($this->user->isCredentialsNonExpired());
+    }
+
+    public function testSerializableInterface()
+    {
+        $this->user
+            ->setUsername('username')
+            ->setPassword('password')
+            ->setSalt('salt')
+            ->setExpired(true)
+            ->setLocked(true)
+            ->setCredentialsExpired(true)
+            ->setEnabled(true)
+            ->addRole('ROLE_ADMIN')
+            ->addRole(new Role('ROLE_USER'))
+        ;
+        $user = new User();
+        $user->unserialize($this->user->serialize());
+        $this->assertEquals($this->user, $user);
+    }
+
+    public function testRolesMethods()
+    {
+        $this->assertEmpty($this->user->getRoles());
+        $this->user->addRole('ROLE_ADMIN');
+        $this->assertArrayHasKey('ROLE_ADMIN', $this->user->getRoles());
+        $this->user->addRole(new Role('ROLE_USER'));
+        $this->assertArrayHasKey('ROLE_USER', $this->user->getRoles());
+        $this->user->removeRole('ROLE_USER');
+        $this->assertArrayNotHasKey('ROLE_USER', $this->user->getRoles());
+    }
+
+    public function testAddRoleMethodException()
+    {
+        $this->setExpectedException('\InvalidArgumentException');
+        $this->user->addRole(1);
+    }
+
+    public function testRemoveRoleMethodException()
+    {
+        $this->setExpectedException('\InvalidArgumentException');
+        $this->user->removeRole(1);
+    }
+
+    public function testIsEqualToMethod()
+    {
+        $this->user
+            ->setUsername('username')
+            ->setPassword('password')
+            ->setSalt('salt');
+        $user = new User();
+        $this->assertFalse($this->user->isEqualTo($user));
+        $user
+            ->setUsername('username')
+            ->setPassword('password')
+            ->setSalt('salt');
+        $this->assertTrue($this->user->isEqualTo($user));
+    }
+}
+
